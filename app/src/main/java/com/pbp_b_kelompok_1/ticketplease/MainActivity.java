42f13abef88ef64b9036a2b3116c015df6ca8062
chrliem <christian.d.setiawan@gmail.com>
2021-11-29 20:08:08 +0700

    Chr 29/11/2021 20:07

---
+++ app/src/main/java/com/pbp_b_kelompok_1/ticketplease/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pbp_b_kelompok_1.ticketplease;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
