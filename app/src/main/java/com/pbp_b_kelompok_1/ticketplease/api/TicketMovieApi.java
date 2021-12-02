d35908be1c27b6b4647645b0223fb31571fcc085
RvL11 <190710062@students.uajy.ac.id>
2021-12-02 19:22:16 +0700

    Ganti API URL

--- app/src/main/java/com/pbp_b_kelompok_1/ticketplease/api/TicketMovieApi.java
+++ app/src/main/java/com/pbp_b_kelompok_1/ticketplease/api/TicketMovieApi.java
@@ -1,11 +1,13 @@
 package com.pbp_b_kelompok_1.ticketplease.api;
 public class TicketMovieApi {
-    public static String BASE_URL = "ini url woe";
+//    public static String BASE_URL = "http://192.168.100.5:8000/api/";
+//    public static String BASE_URL = "url felix";
+    public static String BASE_URL = "http://127.0.0.1:8000/api/";
 
-    public static final String GET_ALL_URL = BASE_URL + "";
-    public static final String GET_BY_ID_URL = BASE_URL + "";
-    public static final String ADD_URL = BASE_URL + "";
-    public static final String UPDATE_URL = BASE_URL + "";
-    public static final String DELETE_URL = BASE_URL + "";
+    public static final String GET_ALL_URL = BASE_URL + "ticket-movie";
+    public static final String GET_BY_ID_URL = BASE_URL + "ticket-movie/";
+    public static final String ADD_URL = BASE_URL + "ticket-movie";
+    public static final String UPDATE_URL = BASE_URL + "ticket-movie/";
+    public static final String DELETE_URL = BASE_URL + "ticket-movie/";
 
 }
